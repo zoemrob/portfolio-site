68ee589a2ec73ce0527fd0bd41d9bd7686d0a1d3
Zoe Robertson <zoe.robertson.m@gmail.com>
2018-11-30 18:56:58 -0800

    feat: removed unnecessary state, converted App to stateless component.
    Signed-off-by: Zoe Robertson <zoe.robertson.m@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
-import {Component} from 'inferno';
 import {Route} from 'inferno-router';
 import {Helmet} from 'inferno-helmet';
-import * as utils from './utils';
 import './App.css';
 import HeaderState from './components/HeaderState';
 import Home from './components/Home/Home';
 import Bio from "./components/Bio/Bio";
 import {prefix} from "./components/ImageContainer";
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            viewportSize: utils.checkWidth(),
-        };
-    }
-
-    _widthHandler = () => {
-        //console.log(this.state.viewportSize);
-        this.setState({viewportSize: utils.checkWidth()});
-    };
-
-    componentDidMount() {
-        window.addEventListener('resize', this._widthHandler);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.viewportSize !== this.state.viewportSize) console.log(this.state.viewportSize);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this._widthHandler);
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <Helmet>
-                    <link rel="prefetch" href={prefix("portland-sign_grmtn4_c_scale,w_1271.jpg")} />
-                </Helmet>
-                <HeaderState />
-                <Route exact path="/" component={Home}/>
-                <Route path="/bio" component={Bio}/>
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div className="App">
+        <Helmet>
+            <link rel="prefetch" href={prefix("portland-sign_grmtn4_c_scale,w_1271.jpg")}/>
+        </Helmet>
+        <HeaderState/>
+        <Route exact path="/" component={Home}/>
+        <Route path="/bio" component={Bio}/>
+    </div>
+);
 
 export default App;
