fe4304c1a987cb488e8fe92388317e6601acc48a
Zoe Robertson <zoe.robertson.m@gmail.com>
2018-12-11 21:26:02 -0800

    feat: initial Project styles
    Signed-off-by: Zoe Robertson <zoe.robertson.m@gmail.com>

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -7,26 +7,30 @@ const makeAlt = title => `A ${title} screenshot.`;
 
 const Project = ({title, desc, repoLink, imgs = [], imgSizes = [], liveLink = ''}) => (
     <li className="project-item">
-        <h3>{title}</h3>
-        {!empty(imgs) && (
-            <ImageContainer
-                figId={makeIdWithMixin(title, 'figure')}
-                imgAlt={makeAlt(title)}
-                imgSrcSet={imgs}
-                imgSizes={imgSizes.length !== 0 ? imgSizes : null}
-                lazy={true}
-            />
-        )}
-        <p className="project-desc">{desc}</p>
-        <div className="repo-icon-holder">
-            <Icon
-                id={makeIdWithMixin(title, 'icon')}
-                name={icons.gh}
-                link={makeRepoLink(repoLink)}
-            />
-            <label className="icon-label" htmlFor={makeIdWithMixin(title, 'icon')}>Repo Link</label>
+        <div className="box-1">
+            <h3 className={empty(imgs) ? 'only-title': null}>{title}</h3>
+            {!empty(imgs) && (
+                <ImageContainer
+                    figId={makeIdWithMixin(title, 'figure')}
+                    imgAlt={makeAlt(title)}
+                    imgSrcSet={imgs}
+                    imgSizes={imgSizes.length !== 0 ? imgSizes : null}
+                    lazy={true}
+                />
+            )}
         </div>
-        {!empty(liveLink) && <a className="live-link" href={liveLink}>Live Example</a>}
+        <div className="box-2">
+            <p className="project-desc">{desc}</p>
+            <div className="repo-icon-holder">
+                <Icon
+                    id={makeIdWithMixin(title, 'icon')}
+                    name={icons.gh}
+                    link={makeRepoLink(repoLink)}
+                />
+                <label className="icon-label" htmlFor={makeIdWithMixin(title, 'icon')}>Repo Link</label>
+            </div>
+            {!empty(liveLink) && <a className="live-link" href={liveLink}>Live Example</a>}
+        </div>
     </li>
 );
 
