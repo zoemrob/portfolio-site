4a2b63474db140231bbfb82d794198909fc59d19
Zoe Robertson <zoe.robertson.m@gmail.com>
2019-01-05 18:31:19 -0800

    feat: added api endpoint for email
    Signed-off-by: Zoe Robertson <zoe.robertson.m@gmail.com>

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
-const api = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : process.env.PUBLIC_URL;
+const api = process.env.NODE_ENV !== 'production' ? 'http://localhost:3001' : 'https://portfolio-email-api.now.sh';
 
 export function postForm(formData) {
     return fetch(api, {
-        //todo:: fix this ish
         method: "POST",
         mode: 'cors',
         headers: {
